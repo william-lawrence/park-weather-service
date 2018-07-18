e2e98a8267f90e00dba3e7ccbf0e00b602f5bf0d
William Lawrence <williamjlawrence1@gmail.com>
2018-07-18 13:29:39 -0400

    added post functionality

--- Capstone.Web/Controllers/HomeController.cs
+++ Capstone.Web/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace Capstone.Web.Controllers
     public class HomeController : Controller
     {
         // Dependency Injection
-
+        
         private readonly IParksDAL ParksDAL;
         private readonly IWeatherDAL WeatherDAL;
         private readonly ISurveyDAL SurveyDAL;
@@ -27,7 +27,21 @@ namespace Capstone.Web.Controllers
         public IActionResult Index()
         {
             return View();
-        }      
+        }  
+        
+        [HttpGet]
+        public IActionResult Survey()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Survey(SurveyResult result)
+        {
+            SurveyDAL.AddNewSurvey(result);
+            return RedirectToAction("Index", "Home");
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
--- Capstone.Web/Models/SurveyResult.cs
+++ Capstone.Web/Models/SurveyResult.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,7 +12,7 @@ namespace Capstone.Web.Models
         /// The id of the survey in the database
         /// </summary>
         public int SurveyID { get; set; }
-        
+
         /// <summary>
         /// The code used to represent the park.
         /// </summary>
@@ -21,7 +22,7 @@ namespace Capstone.Web.Models
         /// The email address of the user that submitted the survey
         /// </summary>
         public string EmailAddress { get; set; }
-        
+
         /// <summary>
         /// The state where the park primarily resides.
         /// </summary>
@@ -31,5 +32,22 @@ namespace Capstone.Web.Models
         /// The activity level of the user that submitted the survey.
         /// </summary>
         public string ActivityLevel { get; set; }
+
+        /// <summary>
+        /// List of all the parks for the drop down menu.
+        /// </summary>
+        public static List<SelectListItem> Parks = new List<SelectListItem>()
+        {
+            new SelectListItem() {Text = "Cuyahoga Valley", Value = "CVNP"},
+            new SelectListItem() {Text = "Everglades", Value = "ENP"},
+            new SelectListItem() {Text = "Grand Canyon", Value = "GCNP"},
+            new SelectListItem() {Text = "Glacier", Value = "GNP"},
+            new SelectListItem() {Text = "Great Smokey Mountains", Value = "GSMNP"},
+            new SelectListItem() {Text = "Grand Teton", Value = "GTNP"},
+            new SelectListItem() {Text = "Mount Rainer", Value = "MRNP"},
+            new SelectListItem() {Text = "Rocky Mountain", Value = "RMNP"},
+            new SelectListItem() {Text = "Yellow Stone", Value = "YNP"},
+            new SelectListItem() {Text = "Yosemite", Value = "YNP2"},
+        };
     }
 }
