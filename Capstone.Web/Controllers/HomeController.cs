b5fe2de74e7bef7ff26e7e10e2d141239f242bfb
Daniel Stone <ds.danstone@gmail.com>
2018-07-20 13:27:42 -0400

    Added weather and map widgets

--- Capstone.Web/Controllers/HomeController.cs
+++ Capstone.Web/Controllers/HomeController.cs
@@ -10,62 +10,64 @@ using Capstone.Web.Extensions;
 
 namespace Capstone.Web.Controllers
 {
-    public class HomeController : Controller
-    {
-        // Dependency Injection
-        #region Dependency Injection
-        private readonly IParksDAL ParksDAL;
-        private readonly IWeatherDAL WeatherDAL;
-        private readonly ISurveyDAL SurveyDAL;
+	public class HomeController : Controller
+	{
+		// Dependency Injection
+		#region Dependency Injection
+		private readonly IParksDAL ParksDAL;
+		private readonly IWeatherDAL WeatherDAL;
+		private readonly ISurveyDAL SurveyDAL;
 
-        public HomeController(IParksDAL parksDAL, IWeatherDAL weatherDAL, ISurveyDAL surveyDAL)
-        {
-            this.ParksDAL = parksDAL;
-            this.WeatherDAL = weatherDAL;
-            this.SurveyDAL = surveyDAL;
-        }
-        #endregion
+		public HomeController(IParksDAL parksDAL, IWeatherDAL weatherDAL, ISurveyDAL surveyDAL)
+		{
+			this.ParksDAL = parksDAL;
+			this.WeatherDAL = weatherDAL;
+			this.SurveyDAL = surveyDAL;
+		}
+		#endregion
 
 		private const string Temp_Unit_Choice = "Temp_Unit_Choice";
 
-        public IActionResult Index()
-        {
-            IList<Park> parks = ParksDAL.GetAllParks();
-            return View(parks);
-        }
+		public IActionResult Index()
+		{
+			IList<Park> parks = ParksDAL.GetAllParks();
+			return View(parks);
+		}
 
-        [HttpGet]
-        public IActionResult Survey()
-        {
-            return View();
-        }
+		[HttpGet]
+		public IActionResult Survey()
+		{
+			return View();
+		}
 
-        [HttpPost]
-        [AutoValidateAntiforgeryToken]
-        public IActionResult Survey(SurveyResult result)
-        {
-            SurveyDAL.AddNewSurvey(result);
-            return RedirectToAction("FavoritePark", "Home");
-        }
+		[HttpPost]
+		[AutoValidateAntiforgeryToken]
+		public IActionResult Survey(SurveyResult result)
+		{
+			SurveyDAL.AddNewSurvey(result);
+			return RedirectToAction("FavoritePark", "Home");
+		}
 
-        [HttpGet]
-        public IActionResult FavoritePark()
-        {
-            var parks = SurveyDAL.GetParkRankings();
-            return View(parks);
-        }
+		[HttpGet]
+		public IActionResult FavoritePark()
+		{
+			var parks = SurveyDAL.GetParkRankings();
+			return View(parks);
+		}
 
 		public IActionResult Detail(string code)
 		{
 			// Get the chosen Park
 			Park park = ParksDAL.GetPark(code);
-			//Get the forecast for the park
-			park.FiveDayForecast = WeatherDAL.GetForecast(code);
-			// If the user has a unit preference for temp unit, apply it to each weather element
-			if (HttpContext.Session.Get<string>(Temp_Unit_Choice) != null)
-			{
-				park.FiveDayForecast.Select(w => { w.UnitPrefence = HttpContext.Session.Get<string>(Temp_Unit_Choice); return w; }).ToList();
-			}
+			////Get the forecast for the park
+			//park.FiveDayForecast = WeatherDAL.GetForecast(code);
+			//// If the user has a unit preference for temp unit, apply it to each weather element
+			//if (HttpContext.Session.Get<string>(Temp_Unit_Choice) != null)
+			//{
+			//	park.FiveDayForecast.Select(w => { w.UnitPrefence = HttpContext.Session.Get<string>(Temp_Unit_Choice); return w; }).ToList();
+			//}
+
+			TempData["unit"] = HttpContext.Session.Get<string>(Temp_Unit_Choice);
 
 			// Only display the page, if the park was found
 			if (park.Code == code)
@@ -80,7 +82,15 @@ namespace Capstone.Web.Controllers
 
 		public IActionResult ChangeUnits(string parkCode, string unit)
 		{
-			HttpContext.Session.Set(Temp_Unit_Choice, unit);
+			string currentUnit = HttpContext.Session.Get<string>(Temp_Unit_Choice);
+			if (currentUnit == null || currentUnit == "F")
+			{
+				HttpContext.Session.Set(Temp_Unit_Choice, "C");
+			}
+			else
+			{
+				HttpContext.Session.Set(Temp_Unit_Choice, "F");
+			}
 			return RedirectToAction("Detail", new { code = parkCode });
 		}
 
