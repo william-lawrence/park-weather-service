abd16421b4b588cebd37cd0c818d8ff482b67d09
William Lawrence <williamjlawrence1@gmail.com>
2018-07-23 09:17:25 -0400

    removed unneccessary JS code from site.js

--- Capstone.Web/wwwroot/js/site.js
+++ Capstone.Web/wwwroot/js/site.js
@@ -3,46 +3,6 @@
 
 // Write your JavaScript code.
 
-//document.onload.setCookie("Unit", "F", 1);
-
-//document.onload.setCookie("Unit", "F", 1);
-
-
-function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    var expires = "expires=" + d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-};
-
-function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-};
-
-function switchCookie() {
-    var unit = getCookie("Unit");
-    if (unit == "F") {
-        setCookie("Unit", "C", 1);
-    } else if (unit == "C") {
-        setCookie("Unit", "F", 1);
-    } else if (unit == "") {
-        setCookie("Unit", "C", 1);
-    }
-
-};
-
-
 function toggleTemp() {
     var buttonTxt = document.getElementById("convertBtn");
     if (buttonTxt.innerHTML == 'Convert To Celsius') {
